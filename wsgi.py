c33c8e844078df99f80227e41f4c46b0f2618727
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-01-06 13:48:55 +0330

    Remove port

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,4 @@
 from api.main import app
 
-from api.config.config import env
-
 if __name__ == "__main__":
-    app.run("0.0.0.0", port=env("PORT"), debug=True)
+    app.run("0.0.0.0", port=8000, debug=True)
