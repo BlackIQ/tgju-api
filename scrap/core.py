116b1539b45c0fd6ba8ff6e5a9a45e5af4a32574
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-07-26 15:11:48 +0330

    Keep scapper to work on and delete wsgi file

---
+++ scrap/core.py
@@ -0,0 +1,66 @@
+import requests
+from bs4 import BeautifulSoup
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+}
+
+def gold():
+    gold_url = "https://www.tgju.org/gold-chart"
+
+    g_r = requests.get(gold_url, headers=headers)
+
+    g_soup = BeautifulSoup(g_r.content, 'html.parser')
+
+    g_tables = g_soup.find_all('table', class_='market-table')
+
+    g_out = []
+
+    for table in g_tables:
+        body = table.find('tbody')
+        rows = body.find_all('tr')
+
+        title = table.find('th').text
+
+        prices = []
+
+        for item in rows:
+            item_title = item.find('th').text
+            item_price = item.find('td', class_='nf').text
+            item_key = item.find_all(
+                'td')[-1].find('a').get('href').split('/')[-1]
+
+            prices.append(
+                {'title': item_title, 'price': item_price, 'key': item_key})
+
+        g_out.append({'title': title, 'prices': prices})
+
+    return g_out
+
+
+def currency():
+    currency_url = "https://www.tgju.org/currency"
+
+    c_r = requests.get(currency_url, headers=headers)
+
+    c_soup = BeautifulSoup(c_r.content, 'html.parser')
+
+    c_tables = c_soup.find_all('table', class_='market-table')
+
+    c_out = []
+
+    for table in c_tables:
+        body = table.find('tbody')
+        rows = body.find_all('tr')
+
+        for row in rows:
+            title = row.find('th').text
+            price = row.find('td', class_='nf').text
+            key = row.find_all('td')[-1].find('a').get('href').split('/')[-1]
+
+            c_out.append({'title': title, 'price': price, 'key': key})
+
+    return c_out
+
+print(gold())
+print(currency())
--- wsgi.py
+++
@@ -1,4 +0,0 @@
-from api.main import app
-
-if __name__ == "__main__":
-    app.run("0.0.0.0", port=8000, debug=True)
