42d780451a525be714aa5a0baccea59a33311fa6
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-07-26 15:40:53 +0330

    Include routers, update metadata for FastAPI

---
+++ main.py
@@ -0,0 +1,23 @@
+# FastAPI
+from fastapi import FastAPI
+
+# Routers
+from routers import price
+
+# FastAPI instance
+app = FastAPI(
+    title="TGJU Scrapper API",
+    version="1.0.1",
+    summary="An API to scrap TGJU website data and share it with you using FastAPI.",
+    contact={
+        "name": "Amirhossein Mohammadi",
+        "url": "https://amirhossein.info",
+        "email": "hi@amirhossein.info",
+    },
+    openapi_tags=[
+        {"name": "Price", "description": "TGJU scrapper endpoints for Gold & Currency"}
+    ],
+)
+
+# Routers
+app.include_router(price.router, prefix="/api")
