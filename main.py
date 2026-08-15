11d53d33bbcf93dfe23a973c1be6d4b37cef018e
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-08-15 18:26:41 +0330

    Cors

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Libs
 from fastapi import FastAPI  # FastAPI
+from fastapi.middleware.cors import CORSMiddleware  # FastAPI CORS
 
 # Application
 from routers import price  # Routers
@@ -18,6 +19,22 @@ app: FastAPI = FastAPI(
         {"name": "Price", "description": "TGJU scrapper endpoints for Gold & Currency"},
     ],
 )
+
+# CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_credentials=True,
+    allow_origins=[
+        "https://tgju.amirhossein.info",
+        "http://localhost:3000",
+        "http://127.0.0.1:8000",
+        "http://localhost:8000",
+        "http://127.0.0.1:3000",
+    ],
+    allow_methods=["GET", "OPTIONS"],
+    allow_headers=["*"],
+)
+
 
 # Routers
 app.include_router(price.router, prefix="/api")
