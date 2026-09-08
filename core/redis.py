e07958ebcdec13b0d98fc4b380a6a820da6b0002
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-09-08 18:13:47 +0330

    Separating redis in 2

--- core/redis.py
+++
@@ -1,7 +0,0 @@
-# Libs
-from redis.asyncio import Redis  # Redis
-
-# Application
-from core.settings import settings  # Core: Settings
-
-redis = Redis.from_url(settings.redis_url, decode_responses=True)
