e376f7982de4f47f9361f188bd9e576f5e8db5f2
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-09-08 17:25:16 +0330

    Just sample of how to get

---
+++ services/tgju.py
@@ -0,0 +1,40 @@
+# Libs
+import json  # JSON
+
+# Application
+from core.redis import redis  # Core: Redis
+from scrap.tgju import (
+    get_currency_prices,
+    get_gold_prices,
+    get_oil_prices,
+)  # Scrapper: TGJU
+
+
+async def currency():
+    data = await get_currency_prices()
+
+    prices = json.dumps([item.model_dump() for item in data], ensure_ascii=False)
+
+    await redis.set("tgju:currency", prices)
+
+
+async def gold():
+    data = await get_gold_prices()
+
+    prices = json.dumps(
+        [item.model_dump() for item in data],
+        ensure_ascii=False,
+    )
+
+    await redis.set("tgju:gold", prices)
+
+
+async def oil():
+    data = await get_oil_prices()
+
+    prices = json.dumps(
+        [item.model_dump() for item in data],
+        ensure_ascii=False,
+    )
+
+    await redis.set("tgju:oil", prices)
