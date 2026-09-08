c78e71528d732222634930c468b6cc2b164612b2
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-09-08 18:14:05 +0330

    Redis Async

---
+++ core/redis_async.py
@@ -0,0 +1,10 @@
+# Libs
+from redis.asyncio import Redis  # Redis
+
+# Application
+from core.settings import settings  # Core: Settings
+
+redis = Redis.from_url(
+    settings.redis_url,
+    decode_responses=True,
+)
